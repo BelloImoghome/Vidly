0125cd2e803cd4e71687a53ef3f28b1b23e8babb
Bello Imoghome <belloimoghome08@gmail.com>
2021-11-11 11:57:12 +0100

    Modified Vidly Website

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -10,6 +10,32 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie
+                {
+                    Id = 1,
+                    Name = "Shrek"
+                },
+
+                new Movie
+                {
+                    Id = 2,
+                    Name = "Wall-E"
+                }
+
+            };
+        }
+
         // GET: Movies/Random
         public ActionResult Random()
         {
@@ -27,8 +53,6 @@ namespace Vidly.Controllers
             };
             return View(viewModel);
         }
-
-
 
         /*
          * 
@@ -56,6 +80,10 @@ namespace Vidly.Controllers
 
             return Content(String.Format("pageIndex = {0} & sortBy = {1}", pageIndex, sortBy));
         }
+
+        
+
+
         */
     }
 }
